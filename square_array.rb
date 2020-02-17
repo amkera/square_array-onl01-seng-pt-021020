925d841d62b2b817ca535848d882343e43a1fa4d
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-02-17 04:41:44 +0000

    Automatically backed up by Learn

---
+++ square_array.rb
@@ -0,0 +1,11 @@
+def square_array(numbers)
+  squared_numbers = []
+  
+  numbers.each do |number|
+    squared_numbers.push(number**2)
+  end
+  return squared_numbers
+end
+
+square_array([1, 2, 3, 4, 5])
+
